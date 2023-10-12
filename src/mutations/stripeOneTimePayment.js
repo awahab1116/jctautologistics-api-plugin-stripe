73afd90bac0edf51c952d78c72f9e2c1feea59bc
awahab1116 <abdul.wahab@codistan.org>
2023-10-12 13:06:07 +0500

    update: decode quoteId

--- src/mutations/stripeOneTimePayment.js
+++ src/mutations/stripeOneTimePayment.js
@@ -1,4 +1,5 @@
 import { createRequire } from "module";
+import { decodeProductOpaqueId } from "../xforms/id.js";
 import ReactionError from "@reactioncommerce/reaction-error";
 const require = createRequire(import.meta.url);
 const stripe = require("stripe")(process.env.STRIPE_API_KEY);
@@ -12,69 +13,76 @@ export default async function stripeOneTimePayment(context, input) {
   //const { product: productInput, shopId, shouldCreateFirstVariant = true } = input;
   const { price, quoteId } = input;
 
-  try {
-    console.log("process env is ", process.env.STRIPE_SUCCESS_URL);
-    console.log("in inner function stripe ", price);
-    console.log("process env is ", process.env.STRIPE_API_KEY);
+  console.log("process env is ", process.env.STRIPE_SUCCESS_URL);
+  console.log("in inner function stripe ", price);
+  console.log("process env is ", process.env.STRIPE_API_KEY);
+  //console.log("condition ", !price || !quoteId.length);
 
-    let findQuote = await Quotes.findOne({
-      _id: quoteId,
-    });
-    console.log("Found Quote is ", findQuote);
-    if (findQuote) {
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price_data: {
-              currency: "usd",
-              product_data: {
-                name: "Stripe payment",
-              },
-              unit_amount: price * 100,
-            },
-            quantity: 1,
-          },
-        ],
-        payment_intent_data: {
-          capture_method: "automatic",
-          metadata: {
-            quoteId,
-            data: "hello",
-          },
-        },
-        mode: "payment",
-        success_url: process.env.STRIPE_SUCCESS_URL,
-        cancel_url: process.env.STRIPE_FAILURE_URL,
-      });
-
-      console.log("Session is ", session);
-
-      let cId = session.id;
-      let stripeCheckoutUrl = session.url;
-
-      let obj = {
-        checkoutSessionId: cId,
-      };
-
-      let updatedQuote = await Quotes.findOneAndUpdate(
-        { _id: quoteId },
-        { $set: obj },
-        { new: true }
-      );
-
-      console.log("Updated Quote is ", updatedQuote);
-
-      return {
-        start: "in stripe",
-        url: stripeCheckoutUrl,
-        stripeJson: JSON.stringify(session),
-        // stripeJson: session,
-      };
-    }
-  } catch (err) {
+  if (!(price && quoteId)) {
+    console.log("here in this");
     throw new ReactionError(
-      "server-error",
-      "Something went wrong in creating a stripe checkout session"
+      "invalid-parameter",
+      "Please provide both price and quoteId"
     );
+  }
+
+  let decodedQuoteid = decodeProductOpaqueId(quoteId);
+  console.log("Decoded quoteId is ", decodedQuoteid);
+
+  let findQuote = await Quotes.findOne({
+    _id: decodedQuoteid,
+  });
+  console.log("Found Quote is ", findQuote);
+  if (findQuote) {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "Stripe payment",
+            },
+            unit_amount: price * 100,
+          },
+          quantity: 1,
+        },
+      ],
+      payment_intent_data: {
+        capture_method: "automatic",
+        metadata: {
+          quoteId,
+          data: "hello",
+        },
+      },
+      mode: "payment",
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_FAILURE_URL,
+    });
+
+    console.log("Session is ", session);
+
+    let cId = session.id;
+    let stripeCheckoutUrl = session.url;
+
+    let obj = {
+      checkoutSessionId: cId,
+    };
+
+    let updatedQuote = await Quotes.findOneAndUpdate(
+      { _id: decodedQuoteid },
+      { $set: obj },
+      { new: true }
+    );
+
+    console.log("Updated Quote is ", updatedQuote);
+
+    return {
+      start: "in stripe",
+      url: stripeCheckoutUrl,
+      stripeJson: JSON.stringify(session),
+      // stripeJson: session,
+    };
+  } else {
+    throw new ReactionError("not-found", "Quote record not found");
   }
 }
--- src/resolvers/Mutation/stripeOneTimePayment.js
+++ src/resolvers/Mutation/stripeOneTimePayment.js
@@ -1,6 +1,6 @@
 export default async function stripeOneTimePayment(_, { input }, context) {
   const { price, quoteId } = input;
-
+  console.log("hello");
   console.log("In stripe one time payment main function ", price, quoteId);
   const response = await context.mutations.stripeOneTimePayment(context, {
     price,
